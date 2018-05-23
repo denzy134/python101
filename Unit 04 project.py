5f3579fa3be62238082dccbb7521436771876062
denzy134 <35541187+denzy134@users.noreply.github.com>
2018-05-23 17:13:26 -0600

    mad lib

---
+++ Unit 04 project.py
@@ -0,0 +1,29 @@
+adjective1 = input("Tell me an adjective, and click enter. ")
+noun1 = input("Tell me a noun, and click enter. ")
+adjective2 = input("Tell me another adjective, and click enter. ")
+verb1 = input("Tell me a verb, and click enter. ")
+noun2 = input("Tell me a noun(plural), and click enter. ")
+noun3 = input("Tell me a noun(plural), and click enter. ")
+noun4 = input("Tell me a noun(plural), and click enter. ")
+noun5 = input("Tell me a noun(plural), and click enter. ")
+verb2 = input("Tell me a verb, and click enter. ")
+noun6 = input("Tell me a noun(plural), and click enter. ")
+verb3 = input("Tell me a verb, and click enter. ")
+verb4 = input("Tell me a verb, and click enter. ")
+noun7 = input("Tell me a noun(plural-food), and click enter. ")
+noun8 = input("Tell me a noun(food), and click enter. ")
+
+
+
+print (" PLANTING A SPRING GARDEN ")
+
+
+print("Spring is a(an) " + adjective1 + " time of year to ")
+print("plant a " + noun1 + " after the") 
+print(adjective2 + " frost. First " + verb1 + " the dirt using ")
+print(noun2 + "and" + noun3 + " so you can plant " + noun4 )
+print(noun5 + verb2 + " when you cover them with " + noun6 + verb3 + "them, ")
+print ("and watch them "+ verb4 + " until summer or fall. " )
+print(noun7 + "grow in a few weeks, whereas Halloween " + noun8 )
+print("may take months, but its worth the wait!")
+print ("             THE END        ")
